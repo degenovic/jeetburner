c2ccfed60d9ba761de018540c33415202ad162a5
frenas <frenasou24@gmail.com>
2025-04-07 21:17:02 +0200

    v2.9.2.5 security updates

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config, { isServer }) => {
+    // Alias bigint-buffer to @trufflesuite/bigint-buffer for security
+    config.resolve.alias['bigint-buffer'] = '@trufflesuite/bigint-buffer';
+    return config;
+  },
 }
 
 module.exports = nextConfig
